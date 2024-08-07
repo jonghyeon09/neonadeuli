8294b58174aa6d738e4678db031382c59f11ab54
jonghyeon <cjddnjfql@naver.com>
2024-08-07 22:39:37 +0900

    fix: 레이아웃

--- app/(heritage)/heritage/layout.tsx
+++ app/(heritage)/heritage/layout.tsx
@@ -2,12 +2,10 @@ import DetailHeader from '@/components/common/DetailHeader';
 
 type Props = {
   children: React.ReactNode;
-  splash: React.ReactNode;
 };
-export default function Layout({ children, splash }: Props) {
+export default function Layout({ children }: Props) {
   return (
     <>
-      {splash}
       <DetailHeader title="국가유산 리스트"></DetailHeader>
       <section className="min-h-screen pt-[var(--h-header)] flex flex-col">
         {children}
